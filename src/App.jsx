3d5ee4787ce9e4be4bf2bd04f7f585a71d2d8d1c
Jeff_김용후 <dydgndlaek97@gmail.com>
2020-08-15 21:55:16 +0900

    feat: preview server test PR

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-  return <div className="App">givven react app</div>;
+  return (
+    <div className="App">
+      givven react app <br /> preview server work?
+    </div>
+  );
 }
 
 export default App;
