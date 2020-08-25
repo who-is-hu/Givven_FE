7696fab796a1cedad0e791761c485217f12924e9
ungs <yachae85@gmail.com>
2020-08-25 17:00:51 +0900

    feat: add 404NotFoundPage

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 import {
   HomePage,
@@ -26,22 +26,25 @@ import {
 function Router() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={HomePage} />
-      <Route path="/myPage/:auth" exact component={MyPage} />
-      <Route path="/dashboard/:auth" exact component={DashBoard} />
-      <Route path="/signIn" exact component={SignInPage} />
-      <Route path="/signUp" exact component={SignUpPage} />
-      <Route path="/market" exact component={MarketPage} />
-      <Route path="/market/:id" exact component={ItemDetailPage} />
-      <Route path="/main" exact component={MainPage} />
-      <Route path="/campaign/:id" exact component={CampaignDetailPage} />
-      <Route path="/transaction" exact component={InquiryTransactionPage} />
-      <Route path="/donate/:id" exact component={DonatePage} />
-      <Route path="/purchase/:id" exact component={PurchasePage}/>
-      <Route path="/registerItem" exact component={RegisterItemPage} />
-      <Route path="/registerCampaign" exact component={RegisterCamgaignPage} />
-      <Route path="/purchaseList/:id" exact component={PurchaseListPage} />
-      <Route path="/order/:id" exact component={OrderPage} />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/myPage/:auth" exact component={MyPage} />
+        <Route path="/dashboard/:auth" exact component={DashBoard} />
+        <Route path="/signIn" exact component={SignInPage} />
+        <Route path="/signUp" exact component={SignUpPage} />
+        <Route path="/market" exact component={MarketPage} />
+        <Route path="/market/:id" exact component={ItemDetailPage} />
+        <Route path="/main" exact component={MainPage} />
+        <Route path="/campaign/:id" exact component={CampaignDetailPage} />
+        <Route path="/transaction" exact component={InquiryTransactionPage} />
+        <Route path="/donate/:id" exact component={DonatePage} />
+        <Route path="/purchase/:id" exact component={PurchasePage}/>
+        <Route path="/registerItem" exact component={RegisterItemPage} />
+        <Route path="/registerCampaign" exact component={RegisterCamgaignPage} />
+        <Route path="/purchaseList/:id" exact component={PurchaseListPage} />
+        <Route path="/order/:id" exact component={OrderPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
     </BrowserRouter>
   );
 }
