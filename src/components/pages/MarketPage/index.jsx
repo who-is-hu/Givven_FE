58d3584a6bcad828e31eb828e2a74f9295ab0dbc
ungs <yachae85@gmail.com>
2020-08-26 17:46:18 +0900

    feat: add calling all items api
    fix: delete console.log

--- src/components/pages/MarketPage/index.jsx
+++ src/components/pages/MarketPage/index.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import Layout from 'components/layout';
+import axios from 'axios';
+
+const CallAllItems = async data => {
+  await axios
+    .get('/item/items')
+    .then(rsp => {
+      console.log(rsp);
+    })
+    .catch(e => console.error(e));
+};
 
 function MarketPage() {
+  CallAllItems();
   return (
     <Layout>
       <div>생젝</div>
