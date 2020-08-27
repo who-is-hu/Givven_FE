cd393a3b6e00e69c72dc4c09525b37de863e500e
ungs <yachae85@gmail.com>
2020-08-27 19:18:09 +0900

    feat: add calling myitems api

--- src/components/pages/DashBoardPage/SellerDashboard/index.jsx
+++ src/components/pages/DashBoardPage/SellerDashboard/index.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from 'components/layout';
+import axios from 'axios';
+import ItemCard from './itemcard';
 
 function SellerDashboard() {
+  const [itemArr, SetItemArr] = useState([]);
+  const [isLoading, SetIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get('/item/myItems').then(rsp => {
+      SetItemArr(rsp.data.data);
+      SetIsLoading(false);
+      console.log(itemArr);
+    });
+  }, []);
+
   return (
     <Layout>
       <div>판매자 마이페이지</div>
+      <div>
+        내 아이템 리스트
+        {isLoading ? (
+          <div> loading </div>
+        ) : (
+          itemArr.map(item => (
+            <ItemCard
+              key={item.id}
+              title_img={item.title_img}
+              name={item.name}
+              price={item.price}
+              stock={item.stock}
+            />
+          ))
+        )}
+      </div>
     </Layout>
   );
 }
