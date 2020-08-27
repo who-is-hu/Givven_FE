96549a36c3f01353676ea477262afb6fe66cb2bb
ungs <yachae85@gmail.com>
2020-08-27 23:10:15 +0900

    feat: added ItemCard css and onClick event

--- src/components/atom/ItemCard/index.jsx
+++ src/components/atom/ItemCard/index.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router';
+import './index.css';
 
-function ItemCard({ titleImg, name, price, stock }) {
+const ItemCard = ({ id, titleImg, name, price, stock }) => {
+  const history = useHistory();
+
   return (
-    <div>
-      <h1>내 아이템</h1>
+    <div className="itemCard" onClick={() => history.push(`/market/:${id}`)}>
       <img src={titleImg} alt="item_img" />
       <div>{name}</div>
       <div>{price}</div>
       <div>{stock}</div>
     </div>
   );
-}
+};
 
 ItemCard.propTypes = {
+  id: PropTypes.number.isRequired,
   titleImg: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
