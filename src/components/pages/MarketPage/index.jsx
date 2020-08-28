105d9c604da812d22a3b21eae8bd87fd42dd3fc5
ungs <yachae85@gmail.com>
2020-08-28 15:22:03 +0900

    fix: remove api in marketpage

--- src/components/pages/MarketPage/index.jsx
+++ src/components/pages/MarketPage/index.jsx
@@ -1,18 +1,7 @@
 import React from 'react';
-import axios from 'axios';
 import Layout from 'components/layout';
 
-const CallAllItems = async data => {
-  await axios
-    .get('/item/items')
-    .then(rsp => {
-      console.log(rsp);
-    })
-    .catch(e => console.error(e));
-};
-
 function MarketPage() {
-  CallAllItems();
   return (
     <Layout>
       <div>생젝</div>
