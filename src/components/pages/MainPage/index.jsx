3383272d100491f8665bd9df9f218bc0f01623e0
ungs <yachae85@gmail.com>
2020-08-28 23:25:05 +0900

    feat: added calling ing campaigns api

--- src/components/pages/MainPage/index.jsx
+++ src/components/pages/MainPage/index.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from 'components/layout';
+import axios from 'axios';
+import { CampaignCard } from 'components/atom';
 
 function MainPage() {
+  const [campaignArr, SetCampaignArr] = useState([]);
+
+  useEffect(() => {
+    axios.get('/campaign/campaigns/ing').then(rsp => {
+      SetCampaignArr(rsp.data);
+    });
+  }, []);
+
   return (
     <Layout>
       <div>메인페이지</div>
+      {campaignArr.map(campaign => (
+        <CampaignCard
+          key={campaign.id}
+          titleImg={campaign.title_img}
+          name={campaign.name}
+        />
+      ))}
     </Layout>
   );
 }
