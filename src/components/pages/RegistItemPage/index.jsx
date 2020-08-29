3c156a96c5a0e120dd720e82d03cde6aad831a24
whoishu <dydgndlaek97@gmail.com>
2020-08-29 18:40:27 +0900

    fix: change name and path

---
+++ src/components/pages/RegistItemPage/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from 'components/layout';
+
+function RegistItemPage() {
+  return (
+    <Layout>
+      <span>상품등록 페이지</span>
+    </Layout>
+  );
+}
+
+export default RegistItemPage;
