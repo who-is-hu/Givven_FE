1c3b82779f0061c65b0605b37c8e3c219f2c7fdd
whoishu <dydgndlaek97@gmail.com>
2020-08-30 17:46:57 +0900

    feat: add UI

--- src/components/pages/SignInPage/index.jsx
+++ src/components/pages/SignInPage/index.jsx
@@ -7,10 +7,75 @@ import axios from 'axios';
 
 import Layout from 'components/layout';
 
-const StyledFrom = styled.form`
+const Container = styled.div`
   display: flex;
   flex-direction: column;
-  width: 500px;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 85vh;
+  padding: 32px 32px;
+`;
+
+const Title = styled.h2`
+  font-size: 2.5rem;
+  color: #00408b;
+  align-self: flex-start;
+  margin-bottom: 80px;
+`;
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+`;
+
+const StyledInput = styled.input`
+  width: 100%;
+  height: 52px;
+  background-color: #fff;
+  border: 1px solid #01656b;
+  border-radius: 15px;
+  padding: 12px 20px;
+
+  ::placeholder {
+    font-size: 1.0625rem;
+    letter-spacing: -0.015em;
+  }
+
+  & ~ & {
+    margin-top: 20px;
+  }
+
+  :focus {
+    outline: none;
+  }
+`;
+
+const StyledButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 52px;
+  margin-top: 20px;
+  cursor: pointer;
+  font-size: 1.5rem;
+  color: #ffffff;
+  background: #00408b;
+  border: none;
+  transition: all 300ms ease;
+
+  :focus {
+    outline: none;
+  }
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const SignupButton = styled(StyledButton)`
+  background: #005ea3;
 `;
 
 const SignInPage = () => {
@@ -39,22 +104,27 @@ const SignInPage = () => {
 
   return (
     <Layout>
-      로그인페이지
-      <StyledFrom onSubmit={handleSubmit(onSubmit)}>
-        <input
-          ref={register}
-          type="text"
-          name="email"
-          placeholder="이메일을 입력해주세요"
-        />
-        <input
-          ref={register}
-          type="password"
-          name="password"
-          placeholder="비밀번호를 입력해주세요"
-        />
-        <button type="submit">submit</button>
-      </StyledFrom>
+      <Container>
+        <StyledForm onSubmit={handleSubmit(onSubmit)}>
+          <Title>Login</Title>
+          <StyledInput
+            ref={register}
+            type="text"
+            name="email"
+            placeholder="이메일"
+          />
+          <StyledInput
+            ref={register}
+            type="password"
+            name="password"
+            placeholder="비밀번호"
+          />
+          <StyledButton type="submit">로그인</StyledButton>
+          <SignupButton type="button" onClick={() => history.push('/signUp')}>
+            회원가입
+          </SignupButton>
+        </StyledForm>
+      </Container>
     </Layout>
   );
 };
