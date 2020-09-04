7e764a3a663c71db66b10541927a9e1a1d99b732
whoishu <dydgndlaek97@gmail.com>
2020-09-04 22:13:30 +0900

    feat: add purchase function

--- src/components/pages/PurchasePage/index.jsx
+++ src/components/pages/PurchasePage/index.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
@@ -8,7 +9,7 @@ import axios from 'axios';
 import commaNumber from 'comma-number';
 
 import Layout from 'components/layout';
-import { Container, Modal, SpinnerScreen } from 'components/atom';
+import { Container, Modal, SpinnerScreen, CampaignCard } from 'components/atom';
 import DaumPostcode from 'react-daum-postcode';
 
 const Title = styled.h1`
@@ -22,7 +23,7 @@ const Title = styled.h1`
 
 const ItemBox = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
 
   width: 100%;
@@ -53,7 +54,7 @@ const ItemInfoRow = styled.div`
 const ItemInfo = styled.div`
   display: flex;
   flex-direction: column;
-  width: 600px;
+  width: 400px;
   margin-left: 32px;
 
   & > h2 {
@@ -96,18 +97,80 @@ const Button = styled.div`
   color: #009ba5;
   border: 2px solid #009ba5;
   border-radius: 10px;
-
   align-self: flex-end;
+`;
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: px 0;
+  width: 100%;
+  p {
+    font-size: 0.8rem;
+    margin-top: 4px;
+  }
+  span {
+    color: coral;
+    font-weight: 500;
+  }
+  transition: all 300ms ease;
+  background-color: ${props =>
+    props.selected ? 'rgba(116, 187, 194, 0.5)' : '#fff'};
+`;
+
+const SectionTitle = styled.h2`
+  font-size: 1.5rem;
+  align-self: flex-start;
+  margin-top: 40px;
+`;
+
+const CampaignList = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: 1fr;
+  gap: 32px;
+  max-height: ${props => (props.drawer ? '9999px' : '300px')};
+  overflow: hidden;
+  transition: all 600ms ease;
+`;
+const NoneNotice = styled.div`
+  margin: auto;
+  font-size: 1.2rem;
+  color: grey;
+`;
+
+const PurchaseBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 8px 16px;
+  border: 2px solid #00427e;
+  color: #00427e;
+  background-color: white;
+  width: 300px;
+  font-size: 1.5rem;
+  font-weight: 700;
+  margin-top: 40px;
+  cursor: pointer;
+  :focus {
+    outline: none;
+  }
 `;
 
 function PurchasePage() {
   const { id } = useParams();
   const [count, setCount] = useState(1);
   const [addr, setAddr] = useState('');
-  const [item, setItem] = useState('');
+  const [item, setItem] = useState([]);
+  const [campaigns, setCampaigns] = useState([]);
+  const [selectedCampaign, setSelectedCampaign] = useState('');
   const [modal, setModal] = useState(false);
+
   const loading = useSelector(state => state.shared.loading);
   const dispatch = useDispatch();
+  const history = useHistory();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -124,8 +187,17 @@ function PurchasePage() {
           alert(err.response);
           dispatch({ type: 'SET_LOADING', loading: false });
         });
+
+      await axios
+        .get('/campaign/myCampaigns/end')
+        .then(rsp => {
+          console.log(rsp);
+          setCampaigns(rsp.data);
+        })
+        .catch(err => {
+          console.log(err.reponse);
+        });
     };
-    console.log('h9');
     fetchData();
   }, []);
 
@@ -157,7 +229,7 @@ function PurchasePage() {
               </ItemInfoRow>
               <ItemInfoRow>
                 <p>구매 총액</p>
-                <p>{commaNumber(count * item.price)}</p>
+                <p>{commaNumber(count * item.price)} 원</p>
               </ItemInfoRow>
               <ItemInfoRow>
                 <p>배송주소</p>
@@ -168,6 +240,66 @@ function PurchasePage() {
               <Button onClick={() => setModal(true)}>주소 찾기</Button>
             </ItemInfo>
           </ItemBox>
+          <SectionTitle>결제할 캠페인</SectionTitle>
+          <CampaignList>
+            {campaigns.length === 0 ? (
+              <NoneNotice />
+            ) : (
+              campaigns.map(campaign => (
+                <CardWrapper
+                  key={campaign.id}
+                  selected={campaign.id === selectedCampaign}
+                  onClick={() => {
+                    setSelectedCampaign(campaign.id);
+                  }}
+                >
+                  <CampaignCard
+                    id={campaign.id}
+                    name={campaign.name}
+                    titleImg={campaign.title_img}
+                    destMoney={campaign.dest_money}
+                    currentMoney={campaign.current_money}
+                    offOnClick
+                    onClick={() => {
+                      setSelectedCampaign(campaign.id);
+                    }}
+                  />
+                  <p>남은 금액: {commaNumber(campaign.current_money)}</p>
+                  <p>
+                    결제 후 금액:
+                    <span>
+                      {commaNumber(campaign.current_money - item.price * count)}
+                    </span>
+                  </p>
+                </CardWrapper>
+              ))
+            )}
+          </CampaignList>
+          <PurchaseBtn
+            onClick={async () => {
+              dispatch({ type: 'SET_LOADING', loading: true });
+              await axios
+                .post('/item/buy', {
+                  itemId: id,
+                  orderCount: count,
+                  campaignId: selectedCampaign,
+                  addr,
+                })
+                .then(rsp => {
+                  console.log(rsp);
+                  alert('구매 완료');
+                  dispatch({ type: 'SET_LOADING', loading: false });
+                  history.goBack();
+                })
+                .catch(err => {
+                  console.log(err.response);
+                  alert(err.response.message);
+                  dispatch({ type: 'SET_LOADING', loading: false });
+                });
+            }}
+          >
+            구매 확정하기
+          </PurchaseBtn>
         </Container>
       </Layout>
       <Modal
